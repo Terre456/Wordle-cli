8c35d3899cd56c7012c4bf0e9e3b3dfb811fca3d
Boris <sabredeboris@gmail.com>
2026-04-02 00:01:59 +0200

    fixed portability

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd ~/Documents/Applications/Wordle1.1
-PATH=$PATH+"/home/$USER/Documents/projets/Wordle/exec-java:"
+#PATH=$PATH+"/home/$USER/Documents/projets/Wordle/exec-java:"
 if [[ ! -d "./tmp" ]]; then
     mkdir tmp
 fi
@@ -14,7 +14,7 @@ echo -e "\033[32mUn caractère en vert est bien placé\033[0m." "\033[33mUn cara
 echo -e "===============================================================================================\n"
 
 win=0
-field=$(exec-java Field $word)
+field=$(./exec-java Field $word)
 
 echo $field | tr " " "\n"
 
