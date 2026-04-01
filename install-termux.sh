5442323359e51ae3a3b60ac029152634242ca63c
Boris <sabredeboris@gmail.com>
2026-04-01 23:52:27 +0200

    fixed install-termux

--- install-termux.sh
+++ install-termux.sh
@@ -1,6 +1,6 @@
 #! /data/data/com.termux/files/usr/bin/bash
 
-if [[ ! -f "$HOME/.profile" ]]; touch $HOME/.profile; chmod +x $HOME/.profile; fi
+if [[ ! -f "$HOME/.profile" ]]; then touch $HOME/.profile; chmod +x $HOME/.profile; fi
 echo "alias wordle=\"bash $HOME/Wordle/main.sh\"" >> $HOME/.profile
 
 mkdir $HOME/Wordle
