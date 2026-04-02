1bdd4a71617e5d5e135dbe42746496649326e6b4
Boris <sabredeboris@gmail.com>
2026-04-02 14:08:21 +0200

    fixed temrux

--- install-termux.sh
+++ install-termux.sh
@@ -3,5 +3,5 @@ cd $(dirname $0)
 if [[ ! -f "$HOME/.profile" ]]; then touch $HOME/.profile; chmod +x $HOME/.profile; fi
 echo "alias wordle=\"bash $HOME/Wordle/main.sh\"" >> $HOME/.profile
 
-if [[ ! d $HOME/Wordle ]]; then mkdir $HOME/Wordle ; fi
+if [[ ! -d $HOME/Wordle ]]; then mkdir $HOME/Wordle ; fi
 cp -r bin Assets lib exec-java main.sh $HOME/Wordle
