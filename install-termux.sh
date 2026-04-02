5ff1b41bc136fc9c331cf04acf07105a7ab53999
Boris <sabredeboris@gmail.com>
2026-04-02 15:38:54 +0200

    fixed termux grep

--- install-termux.sh
+++ install-termux.sh
@@ -1,7 +1,7 @@
 #! /data/data/com.termux/files/usr/bin/bash
 cd $(dirname $0)
 if [[ ! -f "$HOME/.profile" ]]; then touch $HOME/.profile; chmod +x $HOME/.profile; fi
-if test -z $(grep -Pz "(?s)# Wordle commands.*\n.*# Fin Wordle commands" $HOME/.profile) 
+if ! grep -Pqz "(?s)# Wordle commands.*\n.*# Fin Wordle commands" $HOME/.profile
 then echo -e "# Wordle commands\nalias wordle=\"bash $HOME/Wordle/main.sh\"\n# Fin Wordle commands" >> $HOME/.profile
 fi
 
