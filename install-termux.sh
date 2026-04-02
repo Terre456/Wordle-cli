edb87a6bc74b3c79932605664e2b6ec7aedb106a
Boris <sabredeboris@gmail.com>
2026-04-02 14:36:08 +0200

    fixed install-termux

--- install-termux.sh
+++ install-termux.sh
@@ -1,7 +1,7 @@
 #! /data/data/com.termux/files/usr/bin/bash
 cd $(dirname $0)
 if [[ ! -f "$HOME/.profile" ]]; then touch $HOME/.profile; chmod +x $HOME/.profile; fi
-test -z $(grep -Pz "(?s)# Wordle commands.*\n.*# Fin Wordle commands" $HOME/.profile) && echo -e"# Wordle commands\nalias wordle=\"bash $HOME/Wordle/main.sh\"\n# Fin Wordle commands" >> $HOME/.profile
+test -z $(grep -Pz "(?s)# Wordle commands.*\n.*# Fin Wordle commands" $HOME/.profile) && echo -e "# Wordle commands\nalias wordle=\"bash $HOME/Wordle/main.sh\"\n# Fin Wordle commands" >> $HOME/.profile
 
 if [[ ! -d $HOME/Wordle ]]; then mkdir $HOME/Wordle ; fi
 cp -r bin Assets lib exec-java main.sh $HOME/Wordle
