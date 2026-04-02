c519c86f0b831f31f80d802db4a162b2eabdfdeb
Boris <sabredeboris@gmail.com>
2026-04-02 15:09:28 +0200

    main.sh updated

--- main.sh
+++ main.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-alias exec-java="java -cp .:lib/JColor-5.5.1.jar:bin"
+printf "\033[8;25;90t"
+echo -ne "\033]0;Wordle\007"
+cd $(dirname $0)
+exec-java() { 
+    java -cp .:lib/JColor-5.5.1.jar:bin $@ 
+}
 
 if [[ ! -d "./tmp" ]]; then
     mkdir tmp
+    touch tmp/currentwords.txt
 fi
 line=$((1 + $RANDOM % 12825))
-word=$(grep -n -E ".*" Assets/mots4-6.txt | grep -E "^$line:" | cut -d ":" -f 2)
-grep -ow -E ".{${#word}}" Assets/mots4-6.txt > tmp/currentwords.txt
-echo    "==============================================================================================="
+word=$(grep -n -E ".*" "Assets/mots4-6.txt" | grep -E "^$line:" | cut -d ":" -f 2)
+grep -ow -E ".{${#word}}" "Assets/mots4-6.txt" > "tmp/currentwords.txt"
+echo    "=========================================================================================="
 echo    "                       BIENVENUE DANS LE WORDLE DU TERMINAL"
 echo    "            /!\ les accent (é,è,à,â,î,ï,ê,ë,ù,ô) ne sont pas pris en compte /!\ "
-echo -e "\033[32mUn caractère en vert est bien placé\033[0m." "\033[33mUn caractère en jaune est mal placé\033[0m." "\033[31mUn caractère en rouge n'est pas dans le mot\033[0m." 
-echo -e "===============================================================================================\n"
+echo -e "\033[32mUn caractère en vert est bien placé\033[0m."
+echo -e "\033[33mUn caractère en jaune est mal placé\033[0m."
+echo -e "\033[31mUn caractère en rouge n'est pas dans le mot\033[0m."
+echo    "=========================================================================================="
 
 win=0
 field=$(exec-java Field $word)
@@ -19,14 +27,14 @@ field=$(exec-java Field $word)
 echo $field | tr " " "\n"
 
 for i in $(seq 0 5); do
-    echo -e "\n======================================================================\n"
+    echo -e "\n==========================================================================================\n"
     echo -e "Taille du mot a trouver: ${#word} \n"
     echo "Entrez un mot :"
-    read userInputuserInput
+    read userInput
     userInput=$(echo $userInput | tr a-z A-Z |tr -dc A-Z)
 
-    while [ $(grep -cwE $userInput tmp/currentwords.txt) == 0 ]; do
-        echo "Votre mot n'est pas de la bonne logueur ou n'existe pas dans le dictionnaire"
+    while test $(grep -cwE $userInput tmp/currentwords.txt) -eq 0 ; do
+        echo "Votre mot n'est pas de la bonne longueur ou n'existe pas dans le dictionnaire"
         echo "Entrez un mot :"
         read userInput
         userInput=$(echo $userInput | tr a-z A-Z |tr -dc A-Z)
@@ -42,11 +50,11 @@ for i in $(seq 0 5); do
     fi
 done
 
-if [ $win = 1 ] 
+if [[ $win -eq 1 ]] 
 then
-    echo "Bien joué vous avez trouvé !!"
+    echo -e "\033[32mBien joué vous avez trouvé !!\033[32m"
 else 
-    echo "Dommage, c'est raté! le mot était '${word}'."
+    echo -e "\033[31mDommage, c'est raté! le mot était '${word}'.\033[31m"
 fi
 echo -e "Pour rejouer, relancez le programme. \nPour quitter, appuyer sur [ENTREE]"
 read 
